6fc7adf0867a1dcbb2c600c94eb942d2dcd91c66
Anpanprang <88507402+64070030-Nataporn@users.noreply.github.com>
2023-10-18 04:27:53 +0700

    status

--- Medivet/screens/ReminderAppointScreen.js
+++ Medivet/screens/ReminderAppointScreen.js
@@ -1,45 +1,22 @@
-import React, { Component } from "react";
-import { StatusBar } from "expo-status-bar";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Switch,
-  Image,
-  Linking,
-  ScrollView,
-  Button,
-  useWindowDimensions,
-  TouchableOpacity,
-} from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import QueueOwner from "../components/QueueOwner";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { TabView, SceneMap, TabBar } from "react-native-tab-view";
-import {FIREBASE_APP} from "../database/firebaseDB";
+import { FIREBASE_APP } from "../database/firebaseDB";
+import QueueOwner from "../components/QueueOwner";
 
+function ReminderAppoint() {
+  const [queueowner_list, setQueueownerList] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "first", title: "รอยืนยัน" },
+    { key: "second", title: "นัดหมาย" },
+    { key: "third", title: "ประวัติ" },
+    { key: "fourth", title: "เลื่อนนัด" },
+  ]);
 
-class ReminderAppoint extends Component {
-  constructor(props) {
-    super(props);
-    this.queueOwner = FIREBASE_APP.firestore().collection("Appointment");
-    this.state = {
-      queueowner_list: [],
-      index: 0,
-      routes: [
-        { key: "first", title: "รอยืนยัน" },
-        { key: "second", title: "นัดหมาย" },
-        { key: "third", title: "ประวัติ" },
-        { key: "fourth", title: "เลื่อนนัด" },
-      ]
-    };
-    
-  }
-  
-  getCollection = (querySnapshot) => {
-    
+  const getCollection = (querySnapshot) => {
     const all_data = [];
     querySnapshot.forEach((res) => {
-
       const { ClinicID, Date, OwnerID, PetID, Status, Time } = res.data();
       all_data.push({
         key: res.id,
@@ -48,82 +25,69 @@ class ReminderAppoint extends Component {
         OwnerID,
         PetID,
         Status,
-        Time
+        Time,
       });
     });
-    // console.log("all_data : ", all_data);
-    
-    this.setState({
-      queueowner_list: all_data
-    });
+
+    setQueueownerList(all_data);
   };
 
+  useEffect(() => {
+    const unsubscribe = FIREBASE_APP.firestore()
+      .collection("Appointment")
+      .onSnapshot(getCollection);
 
-  componentDidMount() {
-    this.unsubscribe = this.FIREBASE_APP.onSnapshot(this.getCollection);
-  };
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  };
-
-
-  render() {
-    function FirstRoute() {
-      return (
-      <View className="flex-1 bg-white-100 items-center"></View>
-      );
-    }
-    
-    function SecondRoute() {
-      return (
-        <View className="flex-1 bg-white-100 items-center">
-        <QueueOwner></QueueOwner>
-      </View>
-      );
+    return () => {
+      unsubscribe();
     };
-    
-    function ThirdRoute() {
-      return (
-        <View className="flex-1 bg-white-100 items-center"></View>
-      );
-    };
-    
-    function FourthRoute() {
-      return (
-        <View className="flex-1 bg-white-100 items-center"></View>
-      );
-    }; 
+  }, []);
 
-
+  const renderTabBar = (props) => {
     return (
-      <View style={{ flex: 1 }}>
-        <TabView
-          navigationState={this.state}
-          renderScene={SceneMap({
-            first: FirstRoute,
-            second: SecondRoute,
-            third: ThirdRoute,
-            fourth: FourthRoute,
-          })}
-          onIndexChange={(index) => this.setState({ index })}
-          renderTabBar={(props) => this.renderTabBar(props)}
-          initialLayout={{ width: useWindowDimensions().width }}
-        />
-      </View>
+      <TabBar
+        {...props}
+        indicatorStyle={{ backgroundColor: "#379895" }}
+        style={{ backgroundColor: "white" }}
+        labelStyle={{ color: "black" }}
+        activeColor="#379895"
+      />
     );
-  }
+  };
 
-  renderTabBar = (props) => {
-    return (
-    <TabBar
-      {...props}
-      indicatorStyle={{ backgroundColor: "#379895" }}
-      style={{ backgroundColor: "white" }}
-      labelStyle={{ color: "black" }}
-      activeColor="#379895"
-    />
-  )};
+  const FirstRoute = () => (
+    <View className="flex-1 bg-white-100 items-center"></View>
+  );
+
+  const SecondRoute = () => (
+    <View className="flex-1 bg-white-100 items-center">
+      <QueueOwner />
+    </View>
+  );
+
+  const ThirdRoute = () => (
+    <View className="flex-1 bg-white-100 items-center"></View>
+  );
+
+  const FourthRoute = () => (
+    <View className="flex-1 bg-white-100 items-center"></View>
+  );
+
+  return (
+    <View style={{ flex: 1 }}>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={SceneMap({
+          first: FirstRoute,
+          second: SecondRoute,
+          third: ThirdRoute,
+          fourth: FourthRoute,
+        })}
+        onIndexChange={setIndex}
+        renderTabBar={renderTabBar}
+        initialLayout={{ width: useWindowDimensions().width }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -132,21 +96,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  alllist: {
-    width: "100%",
-    alignItems: "center",
-  },
-  logo: {
-    width: 150,
-    height: 200,
-    marginBottom: 0,
-    objectFit: "contain",
-  },
-  header: {
-    fontSize: 28,
-    fontWeight: "bold",
-    marginVertical: 10,
   },
 });
 
