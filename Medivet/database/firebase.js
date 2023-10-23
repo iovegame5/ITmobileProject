e51ec0d621d53dc8624dc1b7123d8b6770893410
Thanakorn Amatawet <64070024@it.kmitl.ac.th>
2023-10-23 14:30:38 +0700

    firebase

---
+++ Medivet/database/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/compat";
+import "firebase/compat/storage"; // Import Firebase Storage
+const firebaseConfig = {
+  apiKey: "AIzaSyAQN4P7C7g0KMoV8rjUfY4pbZrcvvcj5uM",
+  authDomain: "medivet-db13a.firebaseapp.com",
+  projectId: "medivet-db13a",
+  storageBucket: "medivet-db13a.appspot.com",
+  messagingSenderId: "666011955187",
+  appId: "1:666011955187:web:294d636982bf464c752c86"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
